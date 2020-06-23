27d1e3c6a598d21006c8c3ff50123d957dd37117
k-p5w <kos.p5w@gmail.com>
2020-06-24 00:21:58 +0900

    80%くらいできた

--- cmd/md2txt/conv.go
+++ cmd/md2txt/conv.go
@@ -2,29 +2,30 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/url"
 	"os"
 	"path/filepath"
+	"regexp"
 )
 
 // ImportFile is インポートするためのファイル
 type ImportFile struct {
-	pages string
-	data  PageItem
+	Items []PageItem `json:"pages"`
 }
 
 // PageItem is ページデータ
 type PageItem struct {
-	title string
-	lins  []string
+	Title string   `json:"title"`
+	Lins  []string `json:"lines"`
 }
 
 // ReadDir is フォルダ内探索
-func ReadDir(dir string) {
-
+func ReadDir(dir string, tag string) ImportFile {
+	var pg ImportFile
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		panic(err)
@@ -61,29 +62,63 @@ func ReadDir(dir string) {
 				log.Fatal(s.Err())
 			}
 
-			Edittext(lines)
-
+			pageinfo := Edittext(lines, tag)
+			pg.Items = append(pg.Items, pageinfo)
 		}
 	}
 
+	return pg
 }
 
 // Edittext is 本文加工用
-func Edittext(txt []string) string {
+func Edittext(txt []string, tags string) PageItem {
 	//  ここに来るまでには普通のテキストなのでそれをURLエンコードとやらをする。
 	jointxt := ""
-
+	var pi PageItem
 	v := url.Values{}
-
+	//検索条件
+	reg := `^# (.*)`
+	r := regexp.MustCompile(reg)
 	// for ii := 0; ii < len(txt); ii++ {
-	for idx, val := range txt {
+	for _, val := range txt {
+		ret := r.FindStringSubmatch(val)
+		// 0行目で先頭が#だけであればタイトルにする
+		if len(pi.Title) == 0 {
+			pi.Title = ret[1]
+			pi.Lins = append(pi.Lins, ret[1])
+		} else {
 
-		fmt.Printf("行:%v 内容:%v\n", idx, val)
+			pi.Lins = append(pi.Lins, val)
+		}
+		// fmt.Printf("行:%v 内容:%v\n", idx, val)
 		jointxt += val + "\n"
 
 	}
 
+	pi.Lins = append(pi.Lins, tags)
+
 	v.Set("body", jointxt)
 	ret := v.Encode()
-	return ret
+	fmt.Println(ret)
+	return pi
+}
+
+// Writedata is ファイルへの書き出し
+func Writedata(item ImportFile) {
+
+	// jsonエンコード
+	outputJSON, err := json.Marshal(&item)
+	if err != nil {
+		panic(err)
+	}
+
+	//書き込みファイル作成
+	file, err := os.OpenFile("./input.json", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Println(err)
+	}
+	defer file.Close()
+
+	fmt.Fprintln(file, string(outputJSON))
+
 }
--- cmd/md2txt/main.go
+++ cmd/md2txt/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 )
 
 func main() {
 	targetdir := flag.String("d", "dir", "対象フォルダを指定")
+	tag := flag.String("tag", "tag", "一律に設定したい文字列を記載")
 
 	flag.Parse()
-	log.Printf("listening on %q\n", *targetdir)
+	log.Printf("args= %q %q\n", *targetdir, *tag)
 	// フォルダから.mdを読み込む
 	targetdir2 := "C:/home/scrapbox/input"
-	ReadDir(targetdir2)
+	tags := "#映画 "
+	jsondata := ReadDir(targetdir2, tags)
+	fmt.Println(jsondata)
+
+	Writedata(jsondata)
 }
